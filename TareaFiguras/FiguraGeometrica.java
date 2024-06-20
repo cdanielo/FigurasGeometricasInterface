fa1fe16259d0ad9f995604737910338fb331b11d
Carlos Daniel Ocaña Vega <carlosdanielocanavega@gmail.com>
2024-06-20 14:10:24 -0600

    Create FiguraGeometrica.java

---
+++ TareaFiguras/FiguraGeometrica.java
@@ -0,0 +1,11 @@
+
+public interface FiguraGeometrica {
+	public double calcularArea(); 
+	
+	public double calcularPerimetro(); 
+	
+	public String getNombre();
+	
+	public String toString();
+	
+}//interface FiguraGeometrica
