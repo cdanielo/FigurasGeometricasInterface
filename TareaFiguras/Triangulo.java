a85bd3bd4e20e8a00d6cd20d1acebbcadefb70be
Carlos Daniel Ocaña Vega <carlosdanielocanavega@gmail.com>
2024-06-20 14:13:47 -0600

    Create Triangulo.java

---
+++ TareaFiguras/Triangulo.java
@@ -0,0 +1,60 @@
+
+public class Triangulo implements FiguraGeometrica {
+	private String nombre;
+	private double base;
+	private double altura;
+	private double lado;
+	public Triangulo(String nombre, double base, double altura, double lado) {
+		super();
+		this.nombre = nombre;
+		this.base = base;
+		this.altura = altura;
+		this.lado = lado;
+	}//Constructor
+	
+	public double calcularArea() {
+		return (base*altura)/2;
+	}//calcularArea
+	
+	public double calcularPerimetro() {
+		return (lado+lado+lado);
+	}//calcularPerimetro
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getBase() {
+		return base;
+	}
+	public void setBase(double base) {
+		this.base = base;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public double getLado() {
+		return lado;
+	}
+	public void setLado(double lado) {
+		this.lado = lado;
+		
+	}//getters and setters
+	
+	@Override
+	public String toString() {
+		return "Triangulo [nombre=" + nombre + ", base=" + base + ", altura=" + altura + ", lado=" + lado + "]";
+	}//toString
+	
+	
+	
+
+	
+}//class Triangulo
